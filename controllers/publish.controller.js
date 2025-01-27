8be4b4fbe00e84de896160bd056af4649d707834
Emmanuel Frimpong <emmanuelfrimpong07@gmail.com>
2025-01-27 07:59:22 +0000

    Update publish.controller.js

--- controllers/publish.controller.js
+++ controllers/publish.controller.js
@@ -1,25 +1,6 @@
 const PublishService = require('../services/publish.service');
 
 
-//create message
-exports.createMessageTable = async (req, res) => {
-    try {
-        const message = req.body.message;
-        const tables = req.body.tables;
-        console.log("message", tables[0]);
-        if (!message) {
-            res.json({ status: false, message: "Message is required" });
-            return;
-        }
-        if (!tables) {
-            res.json({ status: false, message: "Tables are required" });
-            return;
-        }
-
-        const result1 = await PublishService.saveMessage(message);
-        const result2 = await PublishService.saveTable(tables);
-        res.json({ status: true, message: "Tables published successfully" });
-
 
 //create message
 exports.createMessageTable = async (req, res) => {
@@ -46,57 +27,54 @@ exports.createMessageTable = async (req, res) => {
 }
 //get all messages
 
-//create message
-exports.createMessage = async (req, res) => {
-    try {
-        const message = req.body;
-        const result = await PublishService.saveMessage(message);
-        res.json({ status: true, message: "Data saved", data: result });
+// //create message
+// exports.createMessage = async (req, res) => {
+//     try {
+//         const message = req.body;
+//         const result = await PublishService.saveMessage(message);
+//         res.json({ status: true, message: "Data saved", data: result });
 
-    }
-    catch (err) {
-        res.json({ status: false, message: err.message });
-    }
-}
+//     }
+//     catch (err) {
+//         res.json({ status: false, message: err.message });
+//     }
+// }
 //get all messages
-exports.findAllMessages = async (req, res) => {
-    try {
-        const messages = await PublishService.getMessages();
-        res.json({ status: true, message: "Data found", data: messages });
-    }
-    catch (err) {
-        res.json({ status: false, message: err.message });
-    }
-}
+// exports.findAllMessages = async (req, res) => {
+//     try {
+//         const messages = await PublishService.getMessages();
+//         res.json({ status: true, message: "Data found", data: messages });
+//     }
+//     catch (err) {
+//         res.json({ status: false, message: err.message });
+//     }
+// }
 
 //get all tables by year and semester
 
-exports.findAllTables = async (req, res) => {
-    try {
+// exports.findAllTables = async (req, res) => {
+//     try {
 
 
-        const configId = req.query.configId;
-        if (!configId) {
-            res.json({ status: false, message: "Config Id is required" });
-            return;
-        }
-
-=======
-       
-        const configId = req.query.configId;
-        if(!configId){
-            res.json({ status: false, message: "Config Id is required" });
-            return;
-        }
+//         const configId = req.query.configId;
+//         if (!configId) {
+//             res.json({ status: false, message: "Config Id is required" });
+//             return;
+//         }
+//         const configId = req.query.configId;
+//         if(!configId){
+//             res.json({ status: false, message: "Config Id is required" });
+//             return;
+//         }
        
 
-        const tables = await PublishService.getTables(configId);
-        res.json({ status: true, message: "Data found", data: tables });
-    }
-    catch (err) {
-        res.json({ status: false, message: err.message });
-    }
-}
+//         const tables = await PublishService.getTables(configId);
+//         res.json({ status: true, message: "Data found", data: tables });
+//     }
+//     catch (err) {
+//         res.json({ status: false, message: err.message });
+//     }
+// }
 
 //find table and message 
 exports.findMessageAndTable = async (req, res) => {
@@ -157,4 +135,4 @@ exports.findMessageAndTable = async (req, res) => {
         res.json({ status: false, message: err.message });
 
     }
-};
+}
