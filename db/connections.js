8ad39bef1df396d3b074dacbc8cb6913b41c55d4
Emmanuel Frimpong <emmanuelfrimpong07@gmail.com>
2025-01-27 02:36:12 +0000

    new update with web url

--- db/connections.js
+++ db/connections.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-// const uri =
-//     "mongodb+srv://emmanuelfrimpong07:XjePnIUhA2t9rOM1@timetable.zo6su.mongodb.net/AAMUSTED_TIMETABLE";
+const uri =
+    "mongodb+srv://emmanuelfrimpong07:XjePnIUhA2t9rOM1@timetable.zo6su.mongodb.net/AAMUSTED_TIMETABLE";
 
-const uri = "mongodb://localhost:27017/timetable";
+// const uri = "mongodb://localhost:27017/timetable";
 
 
 // Connect to MongoDB
