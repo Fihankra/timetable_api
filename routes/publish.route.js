ea15da978fce769bf466549d7feb5c774238d84a
Emmanuel Frimpong <emmanuelfrimpong07@gmail.com>
2025-01-27 08:24:04 +0000

    Update publish.route.js

--- routes/publish.route.js
+++ routes/publish.route.js
@@ -6,5 +6,4 @@ const router = express.Router();
 router.post('/message-tables', PublishController.createMessageTable);
 router.get('/messages-tables', PublishController.findMessageAndTable);
 router.get('/messages', PublishController.findAllMessages);
-router.get('/tables', PublishController.findAllTables);
 module.exports = router;
